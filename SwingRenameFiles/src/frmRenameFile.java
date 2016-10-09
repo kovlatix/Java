c05e54586e3346a0e516d2c34c47657591d552d9
kovlatix <kovlatix@gmail.com>
2016-10-09 21:31:54 +0300

    add frmRenameFile

---
+++ SwingRenameFiles/src/frmRenameFile.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by user on 09.10.2016.
+ */
+public class frmRenameFile {
+    private JButton button1;
+
+    public static void main(String[] args) {
+
+    }
+}
