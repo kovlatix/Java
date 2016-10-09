9409f4a61f98602093099e4120705f723b46ba57
kovlatix <kovlatix@gmail.com>
2016-10-09 21:50:06 +0300

    no

--- SwingRenameFiles/src/frmRenameFile.java
+++ SwingRenameFiles/src/frmRenameFile.java
@@ -3,10 +3,14 @@ import javax.swing.*;
 /**
  * Created by user on 09.10.2016.
  */
-public class frmRenameFile {
+public class frmRenameFile extends JFrame {
     private JButton button1;
 
     public static void main(String[] args) {
 
+        frmRenameFile frm1 = new frmRenameFile();
+        frm1.setBounds(10,10,500,400);
+        frm1.setLocationRelativeTo(null);
+        frm1.setVisible(true);
     }
 }
